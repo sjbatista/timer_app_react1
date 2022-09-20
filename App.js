739f619c6fdf8b7c70fd358a30d0e30cdc44446f
sjbatista <salomaojunio@hotmail.com>
2022-09-20 12:59:00 -0300

    Inserted 'LinearGradient', added 'alarmSound'const

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import { useState } from 'react';
 import {Picker} from '@react-native-picker/picker';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
 
@@ -11,18 +12,43 @@ const [hours, setHours] = useState(0);
 
 const [alarmSound, setAlarmSound] = useState([
   {
+    id:1,
     selected: true,
-    song: 'alarm 1',
+    song: 'Sound 1',
     file: 'alarm1.mp3',
   },
   
   {
+    id:2,
     selected: false,
-    song: 'alarm 2',
+    song: 'Sound 2',
     file: 'alarm2.mp3',
+  },
+
+  {
+    id:3,
+    selected: false,
+    song: 'Sound 3',
+    file: 'alarm3.mp3',
   }
 
 ]);
+
+setAlarmClick = (id) => {
+ 
+  let alarmsTemp = alarmSound.map(function(val){
+    if(id != val.id){
+      val.selected = false;
+    }else{
+      val.selected = true;
+    }
+    
+    return val;
+ })
+
+ setAlarmSound(alarmsTemp);
+ 
+}
   
 var numbers = [];
 for(var i=1; i<=60;i++){
@@ -32,13 +58,25 @@ for(var i=1; i<=60;i++){
 
   return (
     <View style={styles.container}>
+      <StatusBar style="auto" />
+      <LinearGradient
+        // Background Linear Gradient
+        colors={['rgba(59,29,105,2.8)', 'rgba(59,29,105,0.7)']}
+        style={{
+          position:'absolute',
+          left:0,
+          right:0,
+          top:0,
+          height:'100%',
+        }}
+      />
 
       <Text style={{color:'white', fontSize:25 }}>Select time interval:</Text>
 
-      <View style={{justifyContent:'space-between', flexDirection:'row', alignItems:'center'}}>
+      <View style={{flexDirection:'row', alignItems:'center', paddingStart:15}}>
       
-        <Text style={{color:'white'}}>H</Text>
-        <Picker style={{color:'white', height:50,width:100}} selectedValue={hours} onValueChange={(itemValue, itemIndex) => setHours(itemValue)}>
+        <Text style={{color:'white', fontSize:12}}>Hours</Text>
+        <Picker style={{color:'white', height:50,width:95}} selectedValue={hours} onValueChange={(itemValue, itemIndex) => setHours(itemValue)}>
           <Picker.Item label="0" value="0" />
             {
               numbers.map(function(val){
@@ -47,8 +85,8 @@ for(var i=1; i<=60;i++){
             } 
         </Picker>
 
-        <Text style={{color:'white'}}>M</Text>
-        <Picker style={{color:'white', height:50,width:100}} selectedValue={minutes} onValueChange={(itemValue, itemIndex) => setMinutes(itemValue)}>
+        <Text style={{color:'white', fontSize:12}}>Minutes</Text>
+        <Picker style={{color:'white', height:50,width:95}} selectedValue={minutes} onValueChange={(itemValue, itemIndex) => setMinutes(itemValue)}>
            <Picker.Item label="0" value="0" />
             {
               numbers.map(function(val){
@@ -57,8 +95,8 @@ for(var i=1; i<=60;i++){
             }
         </Picker>
 
-        <Text style={{color:'white'}}>S</Text>
-        <Picker style={{color:'white', height:50,width:100}} selectedValue={seconds} onValueChange={(itemValue, itemIndex) => setSeconds(itemValue)}>
+        <Text style={{color:'white', fontSize:12}}>Seconds</Text>
+        <Picker style={{color:'white', height:50,width:95}} selectedValue={seconds} onValueChange={(itemValue, itemIndex) => setSeconds(itemValue)}>
             <Picker.Item label="0" value="0" />
             {
               numbers.map(function(val){
@@ -69,7 +107,24 @@ for(var i=1; i<=60;i++){
       
       </View>
 
-      <StatusBar style="auto" />
+      <View style={{flexDirection:'row'}}>
+        {
+
+          alarmSound.map(function(val){
+            if(val.selected == true){
+
+              return(<TouchableOpacity onPress={()=> setAlarmClick(val.id)} style={styles.btnSelected}><Text style={{color:'white'}}>{val.song}</Text></TouchableOpacity>);
+            }
+            else{
+              return(<TouchableOpacity onPress={()=> setAlarmClick(val.id)} style={styles.btnChose}><Text style={{color:'white'}}>{val.song}</Text></TouchableOpacity>);
+            }
+          })
+
+          
+        }
+        
+      </View>
+
     </View>
   );
 
@@ -78,8 +133,22 @@ for(var i=1; i<=60;i++){
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'rgb(80,50,168)',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  btnChose: {
+  marginRight:10,
+  padding:10,
+  backgroundColor:'rgb(116,67,191)'
+  },
+
+  btnSelected: {
+    marginRight:10,
+    padding:10,
+    backgroundColor:'rgba(116,67,191,0.2)',
+    borderColor:'white',
+    borderWidth:1
+  
+    }
 });
